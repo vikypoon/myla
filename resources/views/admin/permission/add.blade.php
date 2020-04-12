094facc78136aa05c82a10794e6c95cc15816ba3
913827497 <913827497@qq.com>
2020-04-12 23:51:35 +0800

    修改permission/add

--- resources/views/admin/permission/add.blade.php
+++ resources/views/admin/permission/add.blade.php
@@ -68,36 +68,38 @@
                 layer = layui.layer;
 
                 //自定义验证规则
-                form.verify({
-                    nikename: function(value) {
-                        if (value.length < 5) {
-                            return '昵称至少得5个字符啊';
-                        }
-                    },
-                    pass: [/(.+){6,12}$/, '密码必须6到12位'],
-                    repass: function(value) {
-                        if ($('#L_pass').val() != $('#L_repass').val()) {
-                            return '两次密码不一致';
-                        }
-                    }
-                });
+                // form.verify({
+                //     nikename: function(value) {
+                //         if (value.length < 5) {
+                //             return '昵称至少得5个字符啊';
+                //         }
+                //     },
+                //     pass: [/(.+){6,12}$/, '密码必须6到12位'],
+                //     repass: function(value) {
+                //         if ($('#L_pass').val() != $('#L_repass').val()) {
+                //             return '两次密码不一致';
+                //         }
+                //     }
+                // });
 
                 //监听提交
                 form.on('submit(add)',
                 function(data) {
                     console.log(data);
                     //发异步，把数据提交给php
-                    layer.alert("增加成功", {
-                        icon: 6
-                    },
-                    function() {
-                        //关闭当前frame
-                        xadmin.close();
 
-                        // 可以对父窗口进行刷新 
-                        xadmin.father_reload();
-                    });
-                    return false;
+                    
+                    // layer.alert("增加成功", {
+                    //     icon: 6
+                    // },
+                    // function() {
+                    //     //关闭当前frame
+                    //     xadmin.close();
+
+                    //     // 可以对父窗口进行刷新 
+                    //     xadmin.father_reload();
+                    // });
+                    // return false;
                 });
 
             });</script>
